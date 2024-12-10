c4c30d160e287f69bef1d4a66a37c34da5fc92be
Guyi Meng <i@mengguyi.com>
2024-12-10 15:58:26 +0800

    重构

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,7 +1,7 @@
 import { hopeTheme } from "vuepress-theme-hope";
+
 import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
-import { MR_HOPE_AVATAR } from "./logo.js";
 
 export default hopeTheme({
   hostname: "https://blog.mengguyi.com",
@@ -21,38 +21,37 @@ export default hopeTheme({
 
   blog: {
     medias: {
-      // Baidu: "https://example.com",
+      //Baidu: "https://example.com",
       BiliBili: "https://space.bilibili.com/474814653",
-      // Bitbucket: "https://example.com",
-      // Dingding: "https://example.com",
-      // Discord: "https://example.com",
-      // Dribbble: "https://example.com",
+      //Bitbucket: "https://example.com",
+      //Dingding: "https://example.com",
+      //Discord: "https://example.com",
+      //Dribbble: "https://example.com",
       Email: "mailto:i@mengguyi.com",
-      // Evernote: "https://example.com",
+      //Evernote: "https://example.com",
       Facebook: "https://www.facebook.com/profile.php?id=100079026494258",
-      // Flipboard: "https://example.com",
+      //Flipboard: "https://example.com",
       Gitee: "https://gitee.com/mengguyi",
       GitHub: "https://github.com/mengguyi",
       Gitlab: "https://gitlab.com/mengguyi",
       Gmail: "mailto:mengguyi.mgy@gmail.com",
-      // Instagram: "https://example.com",
-      // Lark: "https://example.com",
-      // Lines: "https://example.com",
-      // Linkedin: "https://example.com",
-      // Pinterest: "https://example.com",
-      // Pocket: "https://example.com",
-      // QQ: "https://example.com",
-      // Qzone: "https://example.com",
-      // Reddit: "https://example.com",
-      // Rss: "https://example.com",
-      // Steam: "https://example.com",
+      //Instagram: "https://example.com",
+      //Lark: "https://example.com",
+      //Lines: "https://example.com",
+      //Linkedin: "https://example.com",
+      //Pinterest: "https://example.com",
+      //Pocket: "https://example.com",
+      //QQ: "https://example.com",
+      //Qzone: "https://example.com",
+      //Reddit: "https://example.com",
+      //Rss: "https://example.com",
+      //Steam: "https://example.com",
       Twitter: "https://twitter.com/mengguyi",
-      // Wechat: "https://example.com",
+      //Wechat: "https://example.com",
       Weibo: "https://weibo.com/u/7706678689",
-      // Whatsapp: "https://example.com",
+      //Whatsapp: "https://example.com",
       Youtube: "https://www.youtube.com/@mengguyi",
       Zhihu: "https://www.zhihu.com/people/mengguyi",
-      // MrHope: ["https://mister-hope.com", MR_HOPE_AVATAR],
     },
   },
 
@@ -73,13 +72,13 @@ export default hopeTheme({
         intro: "/intro.html",
       },
 
-      // page meta
       metaLocales: {
         editLink: "在 GitHub 上编辑此页",
       },
     },
+
     /**
-     * English locale config
+     * Chinese locale config
      */
     "/en/": {
       // navbar
@@ -97,6 +96,7 @@ export default hopeTheme({
         intro: "/en/intro.html",
       },
 
+      // page meta
       metaLocales: {
         editLink: "Edit this page on GitHub",
       },
@@ -106,100 +106,96 @@ export default hopeTheme({
   // enable it to preview all changes in time
   // hotReload: true,
 
+  // These features are enabled for demo, only preserve features you need here
+  markdown: {
+    align: true,
+    attrs: true,
+    codeTabs: true,
+    component: true,
+    demo: true,
+    figure: true,
+    gfm: true,
+    imgLazyload: true,
+    imgSize: true,
+    include: true,
+    mark: true,
+    plantuml: true,
+    spoiler: true,
+    stylize: [
+      {
+        matcher: "Recommended",
+        replacer: ({ tag }) => {
+          if (tag === "em")
+            return {
+              tag: "Badge",
+              attrs: { type: "tip" },
+              content: "Recommended",
+            };
+        },
+      },
+    ],
+    sub: true,
+    sup: true,
+    tabs: true,
+    tasklist: true,
+    vPre: true,
+
+    // uncomment these if you need TeX support
+    // math: {
+    //   // install katex before enabling it
+    //   type: "katex",
+    //   // or install mathjax-full before enabling it
+    //   type: "mathjax",
+    // },
+
+    // install chart.js before enabling it
+    // chartjs: true,
+
+    // install echarts before enabling it
+    // echarts: true,
+
+    // install flowchart.ts before enabling it
+    // flowchart: true,
+
+    // install mermaid before enabling it
+    // mermaid: true,
+
+    // playground: {
+    //   presets: ["ts", "vue"],
+    // },
+
+    // install @vue/repl before enabling it
+    // vuePlayground: true,
+
+    // install sandpack-vue3 before enabling it
+    // sandpack: true,
+
+    // install @vuepress/plugin-revealjs and uncomment these if you need slides
+    // revealjs: {
+    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+    // },
+  },
+
   plugins: {
     blog: true,
 
-    // install @waline/client before enabling it
-    // WARNING: This is a test server for demo only.
-    // You should create and use your own comment service in production.
+    // Install @waline/client before enabling it
+    // Note: This is for testing ONLY!
+    // You MUST generate and use your own comment service in production.
     // comment: {
     //   provider: "Waline",
     //   serverURL: "https://waline-comment.vuejs.press",
-    // },
-    
-    // comment: {
-    //   provider: "Waline",
-    //   serverURL: "https://blog-comment.mengguyi.com",
     // },
 
     components: {
       components: ["Badge", "VPCard"],
     },
 
-    // all features are enabled for demo, only preserve features you need here
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      figure: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-
-      // install chart.js before enabling it
-      // chart: true,
-
-      // insert component easily
-
-      // install echarts before enabling it
-      // echarts: true,
-
-      // install flowchart.ts before enabling it
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // install katex before enabling it
-      // katex: true,
-
-      // install mathjax-full before enabling it
-      // mathjax: true,
-
-      // install mermaid before enabling it
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
-    },
-
-    // install vuepress-plugin-pwa2 and uncomment these if you want a PWA
+    // install @vuepress/plugin-pwa and uncomment these if you want a PWA
     // pwa: {
     //   favicon: "/favicon.ico",
     //   cacheHTML: true,
-    //   cachePic: true,
+    //   cacheImage: true,
     //   appendBase: true,
     //   apple: {
     //     icon: "/assets/icon/apple-icon-152.png",
