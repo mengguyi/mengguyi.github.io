e28823a5bd456945e91c6fdb16050bf651d7a771
Guyi Meng <i@mengguyi.com>
2024-12-25 17:34:37 +0800

    add comment

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -182,10 +182,10 @@ export default hopeTheme({
     // Install @waline/client before enabling it
     // Note: This is for testing ONLY!
     // You MUST generate and use your own comment service in production.
-    // comment: {
-    //   provider: "Waline",
-    //   serverURL: "https://waline-comment.vuejs.press",
-    // },
+    comment: {
+      provider: "Waline",
+      serverURL: "https://blog-comment.mengguyi.com",
+    },
 
     components: {
       components: ["Badge", "VPCard"],
